2f99684b3c2a679bf0f2697e4e8dbb4eb69b9067
SamehWally <samehwaly27@gmail.com>
2024-09-14 12:25:20 +0300

    starter react project

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="layout">
+      <Header />
+
+      <main>
+        Content
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../css/Footer/Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            &copy; Copyright Sameh Wally 2024
+        </footer>
+    )
+}
+
+export default Footer
